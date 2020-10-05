dc668bf0c8b18660c0a9fbd541b43560624c89db
Garvit Shrestha <shrestha.garvit@wensolutions.com>
2020-10-05 09:44:51 +0545

    changes

--- singular.php
+++ singular.php
@@ -13,51 +13,105 @@ $galaxy_store_layout_type = is_single() ? 'posts_layout' : 'pages_layout';
 ?>
 
 <main class="content">
+
 	<?php galaxy_store_get_breadcrumb(); ?>
 
-	<div class="galaxy-store-single">
-		<div class="container">
-			<div class="row">
+	<div class="page-content">
+		<div class="container clearfix">
 
+			<?php
+			if ( 'left-sidebar' === galaxy_store_get_theme_mod( $galaxy_store_layout_type, 'left-sidebar' ) ) {
+				! galaxy_store_is_woocommerce_page() ? get_sidebar() : null;
+			}
+			?>
 
-				<?php
-				if ( 'left-sidebar' === galaxy_store_get_theme_mod( $galaxy_store_layout_type, 'left-sidebar' ) ) {
-					! galaxy_store_is_woocommerce_page() ? get_sidebar() : null;
-				}
-				?>
+			<div id="primary">
+				<div class="single-blog-post single-blog">
 
-				<div class="col-xl-8">
-					<div class="row">
-
-						<?php
-						while ( have_posts() ) {
-							the_post();
-
-							get_template_part( 'template-parts/content', 'singular' );
-
-							// If comments are open or we have at least one comment, load up the comment template.
-							if ( comments_open() || get_comments_number() ) :
-								comments_template();
-							endif;
-
-						}
-						?>
-
+					<div class="blog-media">
+						<img src="images/blog-img-1.jpg" alt="images">
 					</div>
 
+					<div class="blog-content">
+
+						<div class="user-section">
+							<div class="user-img">
+								<img src="images/human-2.jpg" alt="image">
+							</div>
+							<ul class="blog-page-meta">
+								<li class="author">
+									<a href="#">
+										Admin
+									</a>
+								</li>
+								<li class="date">
+									<a href="#">
+										<i class="icon-calendar"></i> 24 April, 2020
+									</a>
+								</li>
+							</ul>
+						</div>
+
+						<div class="cat-tags">
+							<ul>
+								<li>
+									<a href="#">Fashion</a>
+								</li>
+								<li>
+									<a href="#">Blog</a>
+								</li>
+								<li>
+									<a href="#">Food</a>
+								</li>
+								<li>
+									<a href="#">Doctor</a>
+								</li>
+							</ul>
+						</div>
+
+						<h4 class="blog-title">
+							<a href="#">Country road</a>
+						</h4>
+
+						<!-- contents -->
+						<!-- /contents -->
+
+					</div>
 				</div>
 
-				<?php
-				if ( 'right-sidebar' === galaxy_store_get_theme_mod( $galaxy_store_layout_type, 'right-sidebar' ) ) {
-					! galaxy_store_is_woocommerce_page() ? get_sidebar() : null;
-				}
-				?>
+				<div class="post-navigation">
+					<div class="nav-links">
+						<div class="nav-previous">
+							<span>Prev post</span>
+							<a href="#">70% Discount Approaching On All Kitchen Items</a>
+						</div>
+						<div class="nav-next">
+							<span>Next post</span>
+							<a href="#">Converse Lovers Will Love These New Arrivals</a>
+						</div>
+					</div>
+				</div>
+
+				<div id="comments" class="comments-area">
+					<div id="respond" class="comment-respond">
+						<h3>Leave a Reply</h3>
+					</div>
+				</div>
 
 			</div>
+
+			<?php
+			if ( 'right-sidebar' === galaxy_store_get_theme_mod( $galaxy_store_layout_type, 'right-sidebar' ) ) {
+				! galaxy_store_is_woocommerce_page() ? get_sidebar() : null;
+			}
+			?>
+
 		</div>
 	</div>
 
 </main>
+
+
 
 <?php
 get_footer();
--- template-parts/content-singular.php
+++ template-parts/content-singular.php
@@ -56,3 +56,130 @@
 <?php
 the_post_navigation();
 
+?>
+<div id="primary">
+	<div class="single-blog-post single-blog">
+		<div class="blog-media">
+			<?php the_post_thumbnail( 'full' ); ?>
+		</div>
+		<div class="blog-content">
+
+			<div class="user-section">
+				<div class="user-img">
+					<img src="images/human-2.jpg" alt="image">
+				</div>
+				<ul class="blog-page-meta">
+					<li class="author">
+						<a href="#">
+							Admin
+						</a>
+					</li>
+					<li class="date">
+						<a href="#">
+							<i class="icon-calendar"></i> 24 April, 2020
+						</a>
+					</li>
+				</ul>
+			</div>
+
+			<div class="cat-tags">
+				<ul>
+					<li>
+						<a href="#">Fashion</a>
+					</li>
+					<li>
+						<a href="#">Blog</a>
+					</li>
+					<li>
+						<a href="#">Food</a>
+					</li>
+					<li>
+						<a href="#">Doctor</a>
+					</li>
+				</ul>
+			</div>
+			<h4 class="blog-title">
+				<a href="#">Country road</a>
+			</h4>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum eius expedita hic, vel minima minus reiciendis consequuntur ab beatae necessitatibus amet magni itaque, nostrum vero eos
+				nobis modi temporibus recusandae.
+			</p>
+			<p>
+				Mauris eu molestie dui. Phasellus quis orci nec elit consequat placerat quis quis libero. Phasellus euismod consequat orci a venenatis. Mauris eget tortor nec velit eleifend ultrices a sit amet sem. Proin suscipit sagittis consectetur.
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum eius expedita hic, vel minima minus reiciendis consequuntur ab beatae necessitatibus amet magni itaque, nostrum vero eos
+				nobis modi temporibus recusandae.
+			</p>
+
+			<ul class="wp-block-gallery">
+				<li class="blocks-gallery-item">
+					<figure>
+						<img src="images/fas123.jpg" align="img">
+					</figure>
+				</li>
+				<li class="blocks-gallery-item">
+					<figure>
+						<img src="images/fas124.jpg" align="img">
+					</figure>
+				</li>
+				<li class="blocks-gallery-item">
+					<figure>
+						<img src="images/fas125.jpg" align="img">
+					</figure>
+				</li>
+			</ul>
+
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum eius expedita hic, vel minima minus reiciendis consequuntur ab beatae necessitatibus amet magni itaque, nostrum vero eos
+				nobis modi temporibus recusandae.
+			</p>
+
+			<blockquote class="wp-block-quote">
+				<p>
+					Mauris eu molestie dui. Phasellus quis orci nec elit consequat placerat quis quis libero.
+				</p>
+				<cite>Demo team</cite>
+			</blockquote>
+
+			<p>
+				Mauris eu molestie dui. Phasellus quis orci nec elit consequat placerat quis quis libero. Phasellus euismod consequat orci a venenatis. Mauris eget tortor nec velit eleifend ultrices a sit amet sem. Proin suscipit sagittis consectetur.
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum eius expedita hic, vel minima minus reiciendis consequuntur ab beatae necessitatibus amet magni itaque, nostrum vero eos
+				nobis modi temporibus recusandae.
+			</p>
+
+			<h3>EXAMPLE HEADING</h3>
+
+			<p>
+				Mauris eu molestie dui. Phasellus quis orci nec elit consequat placerat quis quis libero. Phasellus euismod consequat orci a venenatis. Mauris eget tortor nec velit eleifend ultrices a sit amet sem. Proin suscipit sagittis consectetur.
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum eius expedita hic, vel minima minus reiciendis consequuntur ab beatae necessitatibus amet magni itaque, nostrum vero eos
+				nobis modi temporibus recusandae.
+			</p>
+		</div>
+	</div>
+
+	<div class="post-navigation">
+		<div class="nav-links">
+			<div class="nav-previous">
+				<span>Prev post</span>
+				<a href="#">70% Discount Approaching On All Kitchen Items</a>
+			</div>
+			<div class="nav-next">
+				<span>Next post</span>
+				<a href="#">Converse Lovers Will Love These New Arrivals</a>
+			</div>
+		</div>
+	</div>
+
+	<div id="comments" class="comments-area">
+		<div id="respond" class="comment-respond">
+			<h3>Leave a Reply</h3>
+		</div>
+	</div>
+
+</div>
